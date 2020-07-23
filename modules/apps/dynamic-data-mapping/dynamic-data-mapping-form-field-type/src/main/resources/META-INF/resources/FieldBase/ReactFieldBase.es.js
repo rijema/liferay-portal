7e2001c3fe3e2f80c7e3597032d0a964de8abdf9
Richard Jeremias <ricmartinsrocha@gmail.com>
2020-07-23 13:30:41 -0300

    LPS-111072 adding fieldset and legend tag
    This reverts commit 92469fef3c880c3cf5e51ce72fa49b8532f84d8b

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/FieldBase/ReactFieldBase.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/FieldBase/ReactFieldBase.es.js
@@ -125,37 +125,41 @@ function FieldBase({
 					</div>
 				)}
 
-				{((label && showLabel) ||
-					required ||
-					tooltip ||
-					repeatable) && (
-					<p
-						className={classNames({
-							'ddm-empty': !showLabel && !required,
-							'ddm-label': showLabel,
-						})}
-					>
-						{label && showLabel && label}
+				<fieldset>
+					{((label && showLabel) ||
+						required ||
+						tooltip ||
+						repeatable) && (
+						<legend>
+							<p
+								className={classNames({
+									'ddm-empty': !showLabel && !required,
+									'ddm-label': showLabel,
+								})}
+							>
+								{label && showLabel && label}
+		
+								{required && (
+									<span className="reference-mark">
+										<ClayIcon symbol="asterisk" />
+									</span>
+								)}
+		
+								{tooltip && (
+									<span className="ddm-tooltip">
+										<ClayIcon
+											data-tooltip-align="right"
+											symbol="question-circle-full"
+											title={tooltip}
+										/>
+									</span>
+								)}
+							</p>
+						</legend>
+					)}
 
-						{required && (
-							<span className="reference-mark">
-								<ClayIcon symbol="asterisk" />
-							</span>
-						)}
-
-						{tooltip && (
-							<span className="ddm-tooltip">
-								<ClayIcon
-									data-tooltip-align="right"
-									symbol="question-circle-full"
-									title={tooltip}
-								/>
-							</span>
-						)}
-					</p>
-				)}
-
-				{children}
+					{children}
+				</fieldset>
 
 				{nestedFields && <Layout rows={getDefaultRows(nestedFields)} />}
 
