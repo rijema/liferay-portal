b25253fb1494c48183100156a034bcc74a159f9e
Murilo Stodolni <murilo.stodolni@liferay.com>
2022-08-26 15:26:04 -0300

    LPS-161317 Remove validation to SystemObject

--- modules/apps/notification/notification-web/src/main/resources/META-INF/resources/js/components/DefinitionOfTerms.tsx
+++ modules/apps/notification/notification-web/src/main/resources/META-INF/resources/js/components/DefinitionOfTerms.tsx
@@ -108,11 +108,7 @@ export function DefinitionOfTerms({baseResourceURL}: IProps) {
 
 	useEffect(() => {
 		API.getObjectDefinitions().then((items) => {
-			const objectDefinitions = items.filter(
-				({system}: ObjectDefinition) => !system
-			);
-
-			setObjectDefinitions(objectDefinitions);
+			setObjectDefinitions(items);
 		});
 	}, []);
 
