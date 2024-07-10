f75b701d6a1a3babe48ea46e255cfe96bf62490b
Richard Jeremias <richard.jeremias@liferay.com>
2024-07-10 10:14:14 -0300

    LPD-28216 Users without permission to edit can at least read the task

--- modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/portlet/MyWorkflowTaskPortlet.java
+++ modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/portlet/MyWorkflowTaskPortlet.java
@@ -8,7 +8,6 @@ package com.liferay.portal.workflow.task.web.internal.portlet;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
-import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.MapUtil;
@@ -19,7 +18,6 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowException;
 import com.liferay.portal.kernel.workflow.WorkflowTask;
-import com.liferay.portal.kernel.workflow.WorkflowTaskManager;
 import com.liferay.portal.kernel.workflow.WorkflowTaskManagerUtil;
 import com.liferay.portal.workflow.comparator.WorkflowComparatorFactory;
 import com.liferay.portal.workflow.manager.WorkflowLogManager;
@@ -135,49 +133,15 @@ public class MyWorkflowTaskPortlet extends MVCPortlet {
 		return false;
 	}
 
-	private void _checkWorkflowTaskViewPermission(
-			RenderRequest renderRequest, ThemeDisplay themeDisplay,
-			WorkflowTask workflowTask)
-		throws PortalException {
+	private boolean _checkWorkflowTaskViewPermission(
+		ThemeDisplay themeDisplay, WorkflowTask workflowTask) {
 
 		long groupId = MapUtil.getLong(
 			workflowTask.getOptionalAttributes(), "groupId",
 			themeDisplay.getSiteGroupId());
 
-		if (_workflowTaskPermission.contains(
-				themeDisplay.getPermissionChecker(), workflowTask, groupId)) {
-
-			renderRequest.setAttribute(WebKeys.WORKFLOW_TASK, workflowTask);
-			renderRequest.setAttribute(
-				WebKeys.WORKFLOW_TASK_READ_ONLY, Boolean.FALSE);
-
-			return;
-		}
-
-		if ((_countUserInstanceTasks(false, themeDisplay) > 0) ||
-			(_countUserInstanceTasks(true, themeDisplay) > 0)) {
-
-			renderRequest.setAttribute(WebKeys.WORKFLOW_TASK, workflowTask);
-			renderRequest.setAttribute(
-				WebKeys.WORKFLOW_TASK_READ_ONLY, Boolean.TRUE);
-
-			return;
-		}
-
-		throw new PrincipalException.MustHavePermission(
-			themeDisplay.getPermissionChecker(), WorkflowTask.class.getName(),
-			workflowTask.getWorkflowTaskId(), ActionKeys.VIEW);
-	}
-
-	private int _countUserInstanceTasks(
-			boolean taskCompleted, ThemeDisplay themeDisplay)
-		throws WorkflowException {
-
-		return _workflowTaskManager.getWorkflowTaskCountByUser(
-			themeDisplay.getCompanyId(),
-			themeDisplay.getPermissionChecker(
-			).getUserId(),
-			taskCompleted);
+		return _workflowTaskPermission.contains(
+			themeDisplay.getPermissionChecker(), workflowTask, groupId);
 	}
 
 	private void _setWorkflowTaskDisplayContextRenderRequestAttribute(
@@ -203,10 +167,19 @@ public class MyWorkflowTaskPortlet extends MVCPortlet {
 			ThemeDisplay themeDisplay =
 				(ThemeDisplay)renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
 
-			_checkWorkflowTaskViewPermission(
-				renderRequest, themeDisplay,
-				WorkflowTaskManagerUtil.getWorkflowTask(
-					themeDisplay.getCompanyId(), workflowTaskId));
+			WorkflowTask workflowTask = WorkflowTaskManagerUtil.getWorkflowTask(
+				themeDisplay.getCompanyId(), workflowTaskId);
+
+			if (_checkWorkflowTaskViewPermission(themeDisplay, workflowTask)) {
+				renderRequest.setAttribute(WebKeys.WORKFLOW_TASK, workflowTask);
+				renderRequest.setAttribute(
+					WebKeys.WORKFLOW_TASK_READ_ONLY, Boolean.FALSE);
+			}
+			else {
+				renderRequest.setAttribute(WebKeys.WORKFLOW_TASK, workflowTask);
+				renderRequest.setAttribute(
+					WebKeys.WORKFLOW_TASK_READ_ONLY, Boolean.TRUE);
+			}
 		}
 	}
 
@@ -218,9 +191,6 @@ public class MyWorkflowTaskPortlet extends MVCPortlet {
 
 	@Reference
 	private WorkflowLogManager _workflowLogManager;
-
-	@Reference
-	private WorkflowTaskManager _workflowTaskManager;
 
 	@Reference
 	private WorkflowTaskPermission _workflowTaskPermission;
