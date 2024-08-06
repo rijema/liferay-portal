f5b6efee1e2bad515e75ba20c4a132af82fc6c1a
Richard Jeremias <richard.jeremias@liferay.com>
2024-08-06 14:42:13 -0300

    LPD-25352 Considering ctCollectionId when updating calendar booking rows

--- modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/upgrade/v4_2_1/test/UpgradeCalendarBookingTest.java
+++ modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/upgrade/v4_2_1/test/UpgradeCalendarBookingTest.java
@@ -9,22 +9,30 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.calendar.model.Calendar;
 import com.liferay.calendar.model.CalendarBooking;
 import com.liferay.calendar.service.CalendarBookingLocalService;
+import com.liferay.calendar.service.CalendarLocalService;
 import com.liferay.calendar.test.util.CalendarBookingTestUtil;
 import com.liferay.calendar.test.util.CalendarTestUtil;
 import com.liferay.calendar.test.util.CalendarUpgradeTestUtil;
 import com.liferay.calendar.test.util.UpgradeDatabaseTestHelper;
 import com.liferay.calendar.util.JCalendarUtil;
+import com.liferay.change.tracking.test.util.BaseCTUpgradeProcessTestCase;
 import com.liferay.portal.kernel.dao.orm.EntityCacheUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.service.change.tracking.CTService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.junit.After;
@@ -39,7 +47,14 @@ import org.junit.runner.RunWith;
  * @author István András Dézsi
  */
 @RunWith(Arquillian.class)
-public class UpgradeCalendarBookingTest {
+public class UpgradeCalendarBookingTest extends BaseCTUpgradeProcessTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
 
 	@ClassRule
 	@Rule
@@ -55,10 +70,10 @@ public class UpgradeCalendarBookingTest {
 		_upgradeDatabaseTestHelper =
 			CalendarUpgradeTestUtil.getUpgradeDatabaseTestHelper();
 		_upgradeProcess = CalendarUpgradeTestUtil.getUpgradeStep(
-			_upgradeStepRegistrator,
-			"com.liferay.calendar.internal.upgrade.v4_2_1." +
-				"CalendarBookingUpgradeProcess");
+			_upgradeStepRegistrator, _CLASS_NAME);
 		_user = UserTestUtil.addUser();
+
+		_serviceContext = createServiceContext();
 	}
 
 	@After
@@ -70,22 +85,13 @@ public class UpgradeCalendarBookingTest {
 	public void testUpgradeAllDayCalendarBookingStartAndEndTime()
 		throws Exception {
 
-		setUserTimeZoneId("Europe/Paris");
+		CalendarBooking calendarBooking = createCalendarBooking();
 
 		java.util.Calendar expectedStartTimeJCalendar =
-			CalendarFactoryUtil.getCalendar(
-				2022, java.util.Calendar.JANUARY, 1, 0, 0);
+			JCalendarUtil.getJCalendar(calendarBooking.getStartTime());
 
 		java.util.Calendar expectedEndTimeJCalendar =
-			CalendarFactoryUtil.getCalendar(
-				2022, java.util.Calendar.JANUARY, 1, 23, 59);
-
-		ServiceContext serviceContext = createServiceContext();
-
-		CalendarBooking calendarBooking =
-			CalendarBookingTestUtil.addAllDayCalendarBooking(
-				_user, _calendar, expectedStartTimeJCalendar.getTimeInMillis(),
-				expectedEndTimeJCalendar.getTimeInMillis(), serviceContext);
+			JCalendarUtil.getJCalendar(calendarBooking.getEndTime());
 
 		_upgradeProcess.upgrade();
 
@@ -105,6 +111,11 @@ public class UpgradeCalendarBookingTest {
 		assertSameTime(expectedEndTimeJCalendar, actualEndTimeJCalendar);
 	}
 
+	@Override
+	protected CTModel<?> addCTModel() throws Exception {
+		return createCalendarBooking();
+	}
+
 	protected void assertSameTime(
 		java.util.Calendar expectedJCalendar,
 		java.util.Calendar actualJCalendar) {
@@ -119,6 +130,22 @@ public class UpgradeCalendarBookingTest {
 			actualJCalendar.get(java.util.Calendar.MINUTE));
 	}
 
+	protected CalendarBooking createCalendarBooking() throws PortalException {
+		setUserTimeZoneId();
+
+		java.util.Calendar expectedStartTimeJCalendar =
+			CalendarFactoryUtil.getCalendar(
+				2022, java.util.Calendar.JANUARY, 1, 0, 0);
+
+		java.util.Calendar expectedEndTimeJCalendar =
+			CalendarFactoryUtil.getCalendar(
+				2022, java.util.Calendar.JANUARY, 1, 23, 59);
+
+		return CalendarBookingTestUtil.addAllDayCalendarBooking(
+			_user, _calendar, expectedStartTimeJCalendar.getTimeInMillis(),
+			expectedEndTimeJCalendar.getTimeInMillis(), _serviceContext);
+	}
+
 	protected ServiceContext createServiceContext() {
 		ServiceContext serviceContext = new ServiceContext();
 
@@ -128,18 +155,58 @@ public class UpgradeCalendarBookingTest {
 		return serviceContext;
 	}
 
-	protected void setUserTimeZoneId(String timeZoneId) {
-		_user.setTimeZoneId(timeZoneId);
+	@Override
+	protected CTService<?> getCTService() {
+		return _calendarBookingLocalService;
+	}
+
+	@Override
+	protected void runUpgrade() throws Exception {
+		_upgradeProcess.upgrade();
+	}
+
+	protected void setUserTimeZoneId() {
+		_user.setTimeZoneId("Europe/Paris");
 
 		_userLocalService.updateUser(_user);
 	}
 
+	@Override
+	protected CTModel<?> updateCTModel(CTModel<?> ctModel) {
+		_calendarBooking = (CalendarBooking)ctModel;
+
+		java.util.Calendar startTimeJCalendar = CalendarFactoryUtil.getCalendar(
+			2022, java.util.Calendar.JANUARY, 1, 23, 0);
+
+		java.util.Calendar endTimeJCalendar = CalendarFactoryUtil.getCalendar(
+			2022, java.util.Calendar.JANUARY, 2, 22, 59);
+
+		_calendarBooking.setStartTime(startTimeJCalendar.getTimeInMillis());
+		_calendarBooking.setEndTime(endTimeJCalendar.getTimeInMillis());
+
+		_calendarBooking = _calendarBookingLocalService.updateCalendarBooking(
+			_calendarBooking);
+
+		return _calendarBooking;
+	}
+
+	private static final String _CLASS_NAME =
+		"com.liferay.calendar.internal.upgrade.v4_2_1." +
+			"CalendarBookingUpgradeProcess";
+
 	private Calendar _calendar;
+
+	@DeleteAfterTestRun
+	private CalendarBooking _calendarBooking;
 
 	@Inject
 	private CalendarBookingLocalService _calendarBookingLocalService;
 
+	@Inject
+	private CalendarLocalService _calendarLocalService;
+
 	private Group _group;
+	private ServiceContext _serviceContext;
 	private UpgradeDatabaseTestHelper _upgradeDatabaseTestHelper;
 	private UpgradeProcess _upgradeProcess;
 
